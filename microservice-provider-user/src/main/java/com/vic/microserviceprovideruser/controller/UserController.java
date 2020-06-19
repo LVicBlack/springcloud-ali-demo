6c7278748926a75f3efe97a72a0e4b24e146c9e0
liusheng <liusheng@yhbj.cn>
2020-06-19 09:43:08 +0800

    更新

--- microservice-provider-user/src/main/java/com/vic/microserviceprovideruser/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/vic/microserviceprovideruser/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,6 +18,15 @@ public class UserController {
     public User findById(@PathVariable Long id) {
         System.out.println(cache.getCacheByKey(id.toString()));
         return new User(id, "三大", "安安", 22, BigDecimal.ONE);
+    }
+
+    @GetMapping(value = "/user/list", produces = "application/json")
+    public List<User> list() {
+        List<User> result = new ArrayList<>();
+        result.add(new User(1l, "三大", "安安", 22, BigDecimal.ONE));
+        result.add(new User(2l, "三大", "安安", 22, BigDecimal.ONE));
+        result.add(new User(3l, "三大", "安安", 22, BigDecimal.ONE));
+        return result;
     }
 
     @GetMapping(value = "/get", produces = "application/json")
